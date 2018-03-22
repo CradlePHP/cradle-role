adace21a667f3194db776314cb8db35890352b1f
mnlcandelaria <mnl.candelaria@gmail.com>
2018-03-22 20:00:43 +0800

    update helpers

---
+++ .cradle.php
@@ -0,0 +1,8 @@
+<?php //-->
+/**
+ * This file is part of a Custom Package.
+ */
+require_once __DIR__ . '/package/events.php';
+require_once __DIR__ . '/src/events.php';
+require_once __DIR__ . '/src/controller.php';
+require_once __DIR__ . '/package/helpers.php';
--- package/schema/role.php
+++ package/schema/role.php
@@ -3,7 +3,7 @@ return [
     'disable' => '1',
     'singular' => 'Role',
     'plural' => 'Roles',
-    'name' => 'role',
+    'name' => 'auth',
     'icon' => 'fas fa-key',
     'detail' => 'An approach to restricting system access to authorized users',
     'fields' => [
@@ -39,7 +39,7 @@ return [
             'label' => 'Permissions',
             'name' => 'permissions',
             'field' => [
-                'type' => 'meta',
+                'type' => 'json',
             ],
             'validation' => [
                 [
@@ -64,7 +64,7 @@ return [
             'label' => 'Flag',
             'name' => 'flag',
             'field' => [
-                'type' => 'active',
+                'type' => 'int',
             ],
             'list' => [
                 'format' => 'hide',
@@ -154,26 +154,16 @@ return [
             'role_name'         => 'Guest',
             'role_permissions'  => json_encode([
                 [
-                    "path"      => "(?!/admin)/**",
-                    "label"     =>  "Frontend Access",
-                    "method"    => "all"
-                ]
-            ]),
-            'role_created'      => '2018-02-03 01:45:16',
-            'role_updated'      => '2018-02-03 01:45:16',
-            'role_flag'         => 1
-        ],
-        [
-            'role_name'         => 'Rest',
-            'role_permissions'  => json_encode([
+                    "path": "(?!/admin)/**",
+                    "label": "Guest",
+                    "method": "all"
+                ],
                 [
-                    "path"      => "/rest/**",
-                    "label"     => "Rest Access",
-                    "method"    => "all"
+                    "path": "/rest/**",
+                    "label": "Rest",
+                    "method": "all"
                 ]
             ]),
-            'role_created'      => '2018-02-03 01:45:16',
-            'role_updated'      => '2018-02-03 01:45:16',
             'role_flag'         => 1
         ]
     ],
