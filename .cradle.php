fed058fc7528b7ff3dfaa1abf418e1552879cd69
cjzamora <charles.andacc@gmail.com>
2019-01-10 15:32:00 +0800

    update session admin menu if current role is updated

--- .cradle.php
+++ .cradle.php
@@ -16,3 +16,43 @@ $this
     ->preprocess(include __DIR__ . '/src/bootstrap/schema.php')
     ->preprocess(include __DIR__ . '/src/bootstrap/permitted.php')
     ->preprocess(include __DIR__ . '/src/bootstrap/session.php');
+
+//lister for role create/update
+$this->addLogger(function(
+    $message,
+    $request = null,
+    $response = null,
+    $type = null,
+    $table = null,
+    $id = null
+) {
+    //ignore cli
+    if (php_sapi_name() === 'cli') {
+        return;
+    }
+
+    //not role?
+    if ($table !== 'role') {
+        return;
+    }
+
+    //if not create or update
+    if ($type !== 'create' && $type !== 'update') {
+        return;
+    }
+
+    //get current session
+    $session = $request->getSession('role');
+
+    //skip if current role is not updated
+    if ($session['role_id'] !== $id) {
+        return;
+    }
+
+    //get results
+    $results = $response->getResults();
+    $session['role_admin_menu'] = $results['role_admin_menu'];
+
+    //update session
+    $request->setSession('role', $session);
+});
